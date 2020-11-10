405c86027926a3d9b7e2f20354bc6f7128d42163
Anderson Gralha <anderson.gralha@gmail.com>
2020-11-09 22:26:46 -0300

    Add checkbox options

--- Frontend/controller/map.js
+++ Frontend/controller/map.js
@@ -8,6 +8,10 @@ function initMap() {
   });
 }
 
+
+markersRadares = []
+markersPlacas = []
+
 function newMapMark(lat,lng,label,icon) {
 
   var myLatLng = new google.maps.LatLng(lat, lng);
@@ -20,11 +24,47 @@ function newMapMark(lat,lng,label,icon) {
   }
   console.log("pingou mapa " + icon)
   var myMarker = new google.maps.Marker(myMarkerOptions);
+
+  if (icon == "radar") {
+    markersRadares.push(myMarker);
+  } else {
+    markersPlacas.push(myMarker);
+  }
+  
 }
+
 
 function heatMap() {
   heatmap = new google.maps.visualization.HeatmapLayer({
     data: getHeatPoints()
   });
   heatmap.setMap(map);
+}
+
+
+// Radares
+function esconderRadares() {
+  for (let i = 0; i < markersRadares.length; i++) {
+    markersRadares[i].setMap(null);
+  }
+}
+
+function mostrarRadares() {
+  for (let i = 0; i < markersRadares.length; i++) {
+    markersRadares[i].setMap(map);
+  }
+}
+
+
+// Placas
+function esconderPlacas() {
+  for (let i = 0; i < markersPlacas.length; i++) {
+    markersPlacas[i].setMap(null);
+  }
+}
+
+function mostrarPlacas() {
+  for (let i = 0; i < markersPlacas.length; i++) {
+    markersPlacas[i].setMap(map);
+  }
 }
